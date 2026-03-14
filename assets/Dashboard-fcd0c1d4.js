b86073f1049cca9e371c8cf734a37683741329c3
xiaoqu0928 <immortal1889@qq.com>
2026-03-14 14:17:44 +0800

    更新后端地址为 ngrok

--- assets/Dashboard-fcd0c1d4.js
+++ assets/Dashboard-fcd0c1d4.js
@@ -1,4 +1,4 @@
-import{g as LL}from"./admin-9c9c8484.js";import{_ as PL,p as RL,q as EL,r as Ll,o as Pl,c as fm,b as ga,w as ur,e as Bh,F as kL,v as OL,d as hm,a as Si,g as vm,x as NL,y as BL,t as cm}from"./index-d23267c7.js";import"./request-b0373846.js";/*! *****************************************************************************
+import{g as LL}from"./admin-d52486bc.js";import{_ as PL,p as RL,q as EL,r as Ll,o as Pl,c as fm,b as ga,w as ur,e as Bh,F as kL,v as OL,d as hm,a as Si,g as vm,x as NL,y as BL,t as cm}from"./index-9841fae3.js";import"./request-5c71b5d8.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
--- assets/teacher-54dadbc1.js
+++
@@ -1,1 +0,0 @@
-import{n as t}from"./index-d23267c7.js";const r=t("teacher",{state:()=>({token:localStorage.getItem("teacherToken")||"",userInfo:null}),actions:{setToken(e){this.token=e,localStorage.setItem("teacherToken",e)},setUserInfo(e){this.userInfo=e},logout(){this.token="",this.userInfo=null,localStorage.removeItem("teacherToken")}}});export{r as u};
